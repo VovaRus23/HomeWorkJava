7fb195dca55570f4dcc299be2098db244f81e5a0
geekbrains23 <kri5217@yandex.ru>
2022-02-01 14:02:29 +0400

    -a

--- src/HomeWork5/Employee.java
+++ src/HomeWork5/Employee.java
@@ -1,2 +1,31 @@
-package HomeWork5;public class Employee {
+package HomeWork5;
+
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private long phone;
+    private int salary;
+    private int age;
+
+
+    public Employee(String fullName, String position, String email, long phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void getShowInfo() {
+        System.out.println("Fullname: " + fullName + "   " + "Position: " + position + "   " + "Email: " + email + "   " +
+                "Phone: " + phone + "   " + "Salary: " + salary + "   " + "Age: " + age + "\n");
+    }
+
+    public int getShowAge(){
+        return age;
+    }
+
 }
